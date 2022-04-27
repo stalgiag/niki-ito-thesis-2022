4f42d122f51f357f0994734de7e5412a1a720176
Niki Ito <niki.art.ito@gmail.com>
2022-04-27 14:07:10 -0400

    Compress Audio

--- script/preload.js
+++ script/preload.js
@@ -44,17 +44,17 @@ let doorSound;
 
 function preload() {
 
-  optSound = loadSound("./assets/audio/options.wav");
-  nextSlideSound = loadSound("./assets/audio/next.wav");
-  nextChapSound = loadSound("./assets/audio/complete.wav");
-  colorSwitchSound = loadSound("./assets/audio/change-color.wav");
+  optSound = loadSound("./assets/audio/options.mp3");
+  nextSlideSound = loadSound("./assets/audio/next.mp3");
+  nextChapSound = loadSound("./assets/audio/complete.mp3");
+  colorSwitchSound = loadSound("./assets/audio/change-color.mp3");
   talkingSound = loadSound("./assets/audio/mumble.flac");
   BGM = loadSound("./assets/audio/BGM.mp3");
-  rainSound = loadSound("./assets/audio/rain.wav");
-  chewSound = loadSound("./assets/audio/chew.wav");
+  rainSound = loadSound("./assets/audio/rain.mp3");
+  chewSound = loadSound("./assets/audio/chew.mp3");
   btsSound = loadSound("./assets/audio/BTSclip.mp3");
   aniCrossSound = loadSound("./assets/audio/animalCrossing.mp3");
-  doorSound = loadSound("./assets/audio/door-open.wav");
+  doorSound = loadSound("./assets/audio/door-open.mp3");
 
   minaFontB = loadFont("./assets/fonts/mina-bold.ttf");
   minaFontR = loadFont("./assets/fonts/mina-regular.ttf");
