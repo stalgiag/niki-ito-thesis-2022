ce919d8c2564a226c13e64479a3a31b98938de74
Niki Ito <niki.art.ito@gmail.com>
2022-05-19 05:31:13 -0400

    Add code to hide mobile url bar (not sure if working)

--- script/sketch.js
+++ script/sketch.js
@@ -16,6 +16,8 @@ function setup() {
   chapters.push(new Chapter3());
   chapters.push(new Chapter4());
   chapters.push(new Chapter5());
+
+  hideSearchBar();
 }
 
 function draw() {
@@ -61,6 +63,41 @@ function keyPressed() {
     chapters[currentChapterIndex].playAudio();
   }
 }
+
+// hide url bar
+
+function hideSearchBar() {
+    
+  var win = window,
+      doc = win.document;
+
+  // If there's a hash, or addEventListener is undefined, stop here
+  if ( !location.hash || !win.addEventListener ) {
+
+    //scroll to 1
+    window.scrollTo( 0, 1 );
+    var scrollTop = 1,
+
+    //reset to 0 on bodyready, if needed
+    bodycheck = setInterval(function(){
+      if( doc.body ){
+        clearInterval( bodycheck );
+        scrollTop = "scrollTop" in doc.body ? doc.body.scrollTop : 1;
+        win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+      } 
+    }, 15 );
+
+    if (win.addEventListener) {
+      win.addEventListener("load", function(){
+        setTimeout(function(){
+          //reset to hide addr bar at onload
+          win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+        }, 0);
+      }, false );
+    }
+  }
+
+};
 
 
 
